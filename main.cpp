1fde29174f42c66f7d0aabd66175b2b15ed3f972
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-06-03 14:25:06 +0200

    Simplify code after successful tests. Yay!

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ class Force : public Expression
   {
     values[0] = -9.8;  // TODO put some sensible value here
   }
-  
 };
 
 class DirichletBoundary : public SubDomain
@@ -25,38 +24,6 @@ class DirichletBoundary : public SubDomain
     return on_boundary;
   }
 };
-
-// template<typename T>
-void
-dump_full_tensor(const GenericMatrix& A)
-{
-    auto num_rows = A.size(0);
-    auto num_cols = A.size(1);
-
-    std::vector<la_index> rows(num_rows);
-    std::vector<la_index> cols(num_cols);
-    std::iota(rows.begin(), rows.end(), 0);
-    std::iota(cols.begin(), cols.end(), 0);
-    
-    std::vector<double> block(num_rows*num_cols);
-
-    std::cout << "rows: " << num_rows << ", cols: " << num_cols << "\n";
-    
-    // la_index rows[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    // la_index cols[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    // double* block = new double[15*15];
-
-    A.get(block.data(), num_rows, rows.data(), num_cols, cols.data());
-
-    std::cout << std::setprecision(14);
-    for (int i = 0; i < num_rows; i++) {
-      for (int j = 0; j < num_cols; j++) {
-        std::cout << " " << block[i*num_cols + j];
-      }
-      std::cout << "\n";
-    }
-}
-
 
 /* 
  *
@@ -69,41 +36,30 @@ main(void)
   // throws a runtime error.
   dolfin::parameters.add("num_threads", 0);
   
-  // testDKT();
-  //RectangleMesh(Point(0,-pi/2), Point(pi, pi/2), nx, ny, 'crossed')
-  Point p0(0, -M_PI/2);
-  Point p1(M_PI, M_PI/2);
   auto mesh = std::make_shared<RectangleMesh>(MPI_COMM_WORLD,
-                                              p0, p1, 1, 1, "crossed");
+                                              Point (0, -M_PI/2), Point (M_PI, M_PI/2),
+                                              1, 1, "crossed");
   auto W = std::make_shared<LinearKirchhoff::Form_dkt_FunctionSpace_0>(mesh);
   auto Theta = std::make_shared<LinearKirchhoff::Form_p22_FunctionSpace_0>(mesh);
   
   auto u0 = std::make_shared<Constant>(0.0);
   auto boundary = std::make_shared<DirichletBoundary>();
+
   // HermiteDirichletBC bc(W, u0, boundary);
 
   LinearKirchhoff::Form_dkt a(W, W);
   LinearKirchhoff::Form_force L(W);
-  auto b = std::make_shared<LinearKirchhoff::Form_p22>(Theta, Theta);
+  
+  LinearKirchhoff::Form_p22 p22(Theta, Theta);
 
   auto f = std::make_shared<Force>();
   L.f = f;
 
   Function u(W);
 
-  if (true) {
-    KirchhoffAssembler assembler(b);
-    EigenMatrix A;
-    assembler.assemble(A, a);
-    dump_full_tensor(A);
-
-  } else {
-
-    PETScMatrix B;
-    Assembler dolfinAssembler;
-    dolfinAssembler.assemble(B, *b);
-    dump_full_tensor(B);
-  }
-  
+  PETScMatrix A;
+  KirchhoffAssembler assembler;
+  assembler.assemble(A, a, p22);
+    
   return 1;
-}  
+}
